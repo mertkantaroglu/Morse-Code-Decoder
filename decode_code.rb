c07e1371b0175dcd9f1e1ec088ece353d412763d
Mert Kantaroglu <mertkantaroglu@gmail.com>
2023-04-19 17:40:00 +0300

    Fix PR issues

--- decode_code.rb
+++ decode_code.rb
@@ -13,17 +13,17 @@ def decode_char(char)
 end
 
 puts decode_char('--')
+
 # Decode a Word Using decode_char function
 def decode_word(word)
-  decoded_chars = word.split.map { |char| decode_char(char) }
-  decoded_chars.join(' ')
+  word.split.map { |char| decode_char(char) }.join
 end
 
 puts decode_word('-- -.--   -. .- -- .')
 
 # Decode a Sentence Using decode_word function
 def decode_sentence(sentence)
-  decoded_words = sentence.split.map { |word| decode_word(word) }
-  decoded_words.join(' ')
+  sentence.split('  ').map { |word| get_word(word) }.join('  ')
 end
+
 puts decode_sentence('.-  -... --- -..-   ..-. ..- .-.. .-..   --- ..-.  .-. ..- -... .. . ...')
